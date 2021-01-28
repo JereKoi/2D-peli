d68bbfb55a245befbe525862219ad0ba049876bd
TeemuAit <tee.aittola@gmail.com>
2021-01-28 14:00:38 +0200

    testipelin tekeminen

---
+++ Assets/Testipeli/Assets/MonsterData.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class MonsterData : MonoBehaviour
+{
+    private MonsterLevel currentLevel;
+    //1
+    public MonsterLevel CurrentLevel
+    {
+        //2
+        get
+        {
+            return currentLevel;
+        }
+        //3
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+    //Näyttää oikean levelin
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+    //Tornien level up
+    public MonsterLevel GetNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public void IncreaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+
+
+    [System.Serializable]
+    public class MonsterLevel
+    {
+        public int cost;
+        public GameObject visualization;
+    }
+    public List<MonsterLevel> levels;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
